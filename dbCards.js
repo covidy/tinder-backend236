bb5a5a998fa0f97bbc829af157a447d9aa755612
Wilner Vangeliste <vangelistewilner@outlook.fr>
2021-11-09 21:07:00 +0100

    Add files via upload

---
+++ dbCards.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const cardSchema = mongoose.Schema({
+  name: string,
+  imgUrl: string,
+});
+
+
+export default mongoose.model('cards', cardSchema);
---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Cors from 'cors';
+import Cards from './model/dbCards.js';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://admin:123soleil@cluster0.xcnsd.mongodb.net/tinderdb?retryWrites=true&w=majority'
+
+// Middlewares
+app.use(express.json());
+app.use(Cors());
+// DB config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifieldTopology: true,
+})
+// API Endpoints
+app.get("./", (req, res) => res.status(200).send("HELLO WUSK PROGRAMMER!!!"));
+
+app.post('/tinder/card', (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+});
+
+app.get('/tinder/cards', (err, data) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  });
+});
+
+  // Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
